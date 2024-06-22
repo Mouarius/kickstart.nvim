896f74f63160594b462407a024b63b6d05d18b26
Marius Menault <mouarius@gmail.com>
2024-06-22 18:12:27 +0200

    Add astro to formatter options

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -16,6 +16,7 @@ return { -- Autoformat
       lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
       python = { { 'ruff_format', 'black' } },
+      astro = { { 'prettierd', 'prettier' } },
       --
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
