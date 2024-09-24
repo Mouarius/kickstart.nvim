1791ad17b0754761a9293e1809bea69e4f8467b4
Marius Menault <mouarius@gmail.com>
2024-09-24 23:41:54 +0200

    refactor: extract which-key

--- lua/custom/lazy-plugins.lua
+++ lua/custom/lazy-plugins.lua
@@ -5,49 +5,6 @@ require('lazy').setup({
   },
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'christoomey/vim-tmux-navigator', -- Integration with tmux panes
-
-  { -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    event = 'VimEnter',
-    config = function()
-      local wk = require 'which-key'
-      wk.add {
-        { '<leader>c', group = '[C]ode' },
-        { '<leader>c_', hidden = true },
-        { '<leader>d', group = '[D]ocument' },
-        { '<leader>d_', hidden = true },
-        { '<leader>h', group = 'Git [H]unk' },
-        { '<leader>h_', hidden = true },
-        { '<leader>o', group = '[O]ptions' },
-        { '<leader>o_', hidden = true },
-        { '<leader>r', group = '[R]ename' },
-        { '<leader>r_', hidden = true },
-        { '<leader>s', group = '[F]ind' },
-        { '<leader>s_', hidden = true },
-        { '<leader>t', group = '[T]oggle' },
-        { '<leader>t_', hidden = true },
-        { '<leader>w', group = '[W]orkspace' },
-        { '<leader>w_', hidden = true },
-        { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
-      }
-      wk.setup()
-
-      -- require('which-key').register {
-      --   ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      --   ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      --   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      --   ['<leader>s'] = { name = '[F]ind', _ = 'which_key_ignore' },
-      --   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      --   ['<leader>o'] = { name = '[O]ptions', _ = 'which_key_ignore' },
-      --   ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-      --   ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      -- }
-      -- -- visual mode
-      -- require('which-key').register({
-      --   ['<leader>h'] = { 'Git [H]unk' },
-      -- }, { mode = 'v' })
-    end,
-  },
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
---
+++ lua/custom/plugins/which-key.lua
@@ -0,0 +1,36 @@
+return { -- Useful plugin to show you pending keybinds.
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+  config = function()
+    local wk = require 'which-key'
+    wk.add {
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>c_', hidden = true },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>d_', hidden = true },
+      { '<leader>h', group = 'Git [H]unk' },
+      { '<leader>h_', hidden = true },
+      { '<leader>o', group = '[O]ptions' },
+      { '<leader>o_', hidden = true },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>r_', hidden = true },
+      { '<leader>s', group = '[F]ind' },
+      { '<leader>s_', hidden = true },
+      { '<leader>t', group = '[T]oggle' },
+      { '<leader>t_', hidden = true },
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>w_', hidden = true },
+      { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
+    }
+    wk.setup()
+  end,
+  keys = {
+    {
+      '<leader>?',
+      function()
+        require('which-key').show { global = false }
+      end,
+      desc = 'Buffer Local Keymaps (which-key)',
+    },
+  },
+}
