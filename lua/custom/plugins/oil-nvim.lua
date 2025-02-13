a57582a83a6c0198d0c45a51b48499b5af55ed0c
Marius Menault <marius.me@hellowatt.fr>
2025-02-13 19:49:58 +0100

    Fix oil keymaps

--- lua/custom/plugins/oil-nvim.lua
+++ lua/custom/plugins/oil-nvim.lua
@@ -2,9 +2,26 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
-  config = function()
-    require('oil').setup {}
+  opts = {
+    keymaps = {
+      ['<C-S>'] = { 'actions.select', opts = { vertical = true } },
+      ['<C-H>'] = { 'actions.select', opts = { horizontal = true } },
+      ['<C-t>'] = { 'actions.select', opts = { tab = true } },
+      ['<C-p>'] = 'actions.preview',
+      ['<C-c>'] = { 'actions.close', mode = 'n' },
+      ['<C-L>'] = 'actions.refresh',
+      ['-'] = { 'actions.parent', mode = 'n' },
+      ['_'] = { 'actions.open_cwd', mode = 'n' },
+      ['<'] = { 'actions.cd', mode = 'n' },
+      ['>'] = { 'actions.cd', opts = { scope = 'tab' }, mode = 'n' },
+      ['gs'] = { 'actions.change_sort', mode = 'n' },
+      ['gx'] = 'actions.open_external',
+      ['g.'] = { 'actions.toggle_hidden', mode = 'n' },
+      ['g\\'] = { 'actions.toggle_trash', mode = 'n' },
+    },
+  },
+  config = function(_, opts)
+    require('oil').setup(opts)
     vim.keymap.set('n', '-', '<cmd>Oil<cr>', { desc = 'Open parent directory' })
   end,
   -- Optional dependencies
