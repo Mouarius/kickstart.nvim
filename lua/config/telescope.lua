4bc1d35c110c80bf0ea6424b1a1a98e39eed2c2b
Marius Menault <marius.me@hellowatt.fr>
2025-10-06 09:43:58 +0200

    Add new commands to telescope

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -48,7 +48,81 @@ local live_multigrep = function(opts)
     :find()
 end
 
+local live_grep_class = function(opts)
+  opts = opts or {}
+  opts.cwd = opts.cwd or vim.uv.cwd()
+
+  local finder = finders.new_async_job {
+    command_generator = function(prompt)
+      if not prompt or prompt == '' then
+        return nil
+      end
+
+      local args = { 'rg' }
+
+      table.insert(args, '-e')
+      table.insert(args, 'class ' .. prompt)
+
+      ---@diagnostic disable-next-line: deprecated
+      return vim.tbl_flatten {
+        args,
+        { '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case' },
+      }
+    end,
+    entry_maker = make_entry.gen_from_vimgrep(opts),
+    cwd = opts.cwd,
+  }
+
+  pickers
+    .new(opts, {
+      debounce = 100,
+      prompt_title = 'Grep class',
+      finder = finder,
+      previewer = conf.grep_previewer(opts),
+      sorter = require('telescope.sorters').empty(),
+    })
+    :find()
+end
+
+local live_grep_models = function(opts)
+  opts = opts or {}
+  opts.cwd = opts.cwd or vim.uv.cwd()
+
+  local finder = finders.new_async_job {
+    command_generator = function(prompt)
+      if not prompt or prompt == '' then
+        return nil
+      end
+
+      local args = { 'rg' }
+
+      table.insert(args, '-e')
+      table.insert(args, 'class ' .. prompt)
+
+      ---@diagnostic disable-next-line: deprecated
+      return vim.tbl_flatten {
+        args,
+        { '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case', '-g', '**/models/**/*.py', '-g', '**/models.py' },
+      }
+    end,
+    entry_maker = make_entry.gen_from_vimgrep(opts),
+    cwd = opts.cwd,
+  }
+
+  pickers
+    .new(opts, {
+      debounce = 100,
+      prompt_title = 'Grep models',
+      finder = finder,
+      previewer = conf.grep_previewer(opts),
+      sorter = require('telescope.sorters').empty(),
+    })
+    :find()
+end
+
 M.setup = function()
   vim.keymap.set('n', '<leader>fg', live_multigrep)
+  vim.keymap.set('n', '<leader>fc', live_grep_class)
+  vim.keymap.set('n', '<leader>fm', live_grep_models)
 end
 return M
